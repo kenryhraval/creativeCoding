35b96602cc0a7c7a301db3c382aeb24c6efca396
Henrijs Kravals <henrijskravals98@gmail.com>
2024-04-24 22:32:29 +0300

    finished fractal generator

--- parquet.js
+++ parquet.js
@@ -1,18 +1,36 @@
 // plane overlays using the same figure in multiple instances
 
-let grid;
-let w = 1000;
-let h = 1000;
+// change the size of the first cross
+initialSize = 128;
 
+
+let w = 700;
+let h = 700;
+
+let grid;
+
+// change if uniform color is needed for each level
 let randomColors;
 
+const rainbowColors = [
+  '#FF0000', // Red
+  '#FF7F00', // Orange
+  '#FFFF00', // Yellow
+  '#00FF00', // Green
+  '#0000FF', // Blue
+  '#4B0082', // Indigo
+  '#9400D3', // Violet
+  '#800080'  // Purple
+];
 
 
 function setup() {
-  createCanvas(1000 , 1000);
+  createCanvas(w, h);
   background(255);
   
+  // randomColors = generateRandomColors(10);
   randomColors = generateRandomColors(10);
+
   // Initialize the grid
   grid = createGrid(w, h);
 
@@ -25,6 +43,12 @@ function draw() {
   drawGrid();
 }
 
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    saveCanvas(canvas, 'myCanvas', 'png');
+  }
+}
+
 function generateRandomColors(n) {
   let colors = [];
   for (let i = 0; i < n; i++) {
@@ -34,9 +58,9 @@ function generateRandomColors(n) {
   return colors;
 }
 
-// Fill the grid
+
 function fillGrid() {
-    let size = 256;
+    let size = initialSize;
     let currentX = Math.floor(w / 2) - size / 2;
     let currentY = Math.floor(h / 2) - size / 2;
     drawCross(size, currentX, currentY, color(0))
@@ -46,24 +70,17 @@ function fillGrid() {
 function drawCross(size, x, y, col) {
   for (let i = x - size; i < x + 2 * size && i > 0 && i < w; i++) {
       for (let j = y; j < y + size && j > 0 && j < h; j++) {
-        // if (grid[i][j] == 0) {
-
-          // grid[i][j] = col
-          grid[i][j] = randomColors[Math.log2(size)]
-
-        // }
-        // else {pass}  
+          grid[i][j] = col
+          // grid[i][j] = randomColors[Math.log2(size)]
+          // grid[i][j] = rainbowColors[Math.log2(size)]
+ 
       }
   }
   for (let i = y - size; i < y + 2 * size && i > 0 && i < h; i++) {
     for (let j = x; j < x + size && j > 0 && j < w; j++) {
-      // if (grid[i][j] == 0)  {  
-
-        // grid[j][i] = col
-        grid[j][i] = randomColors[Math.log2(size)]
-
-      // }
-      // else {pass}
+        grid[j][i] = col
+        // grid[j][i] = randomColors[Math.log2(size)]
+        // grid[j][i] = rainbowColors[Math.log2(size)]
     }
   }
 
@@ -81,7 +98,6 @@ function drawCross(size, x, y, col) {
 
 }
 
-// Function to create a 2D grid
 function createGrid(rows, cols) {
   let grid = new Array(rows);
   for (let i = 0; i < rows; i++) {
@@ -93,7 +109,6 @@ function createGrid(rows, cols) {
   return grid;
 }
 
-// Function to draw the grid
 function drawGrid() {
   let cellSize = width / grid.length; 
   for (let i = 0; i < grid.length; i++) {
